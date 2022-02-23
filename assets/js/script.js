5a67b44032e75b26fe7bf6afa2da765e7033d2c1
Muhammad Maruf <88553825+MuhammadMaruf11@users.noreply.github.com>
2022-02-24 00:44:30 +0600

    commit

---
+++ assets/js/script.js
@@ -0,0 +1,164 @@
+(function ($) {
+
+    'use strict';
+
+	/*------------------------------------
+		Mobile Menu
+	--------------------------------------*/
+
+	/*-------------------------------------------
+	    Sticky Header
+	--------------------------------------------- */
+
+	let win = $(window);
+	let sticky_id = $(".header-area");
+	win.on('scroll', function () {
+		let scroll = win.scrollTop();
+		if (scroll < 245) {
+			sticky_id.removeClass("sticky-header");
+		} else {
+			sticky_id.addClass("sticky-header");
+		}
+	});
+
+
+	/*------------------------------------
+        Overlay Close
+	--------------------------------------*/
+	$(window).scroll(function () {
+		if ($(this).scrollTop() !== 0) {
+			$('#scrollUp').fadeIn();
+		} else {
+			$('#scrollUp').fadeOut();
+		}
+    });
+    
+	$('#scrollUp').on('click', function () {
+		$("html, body").animate({scrollTop: 0}, 600);
+		return false;
+	});
+
+	/*------------------------------------
+        Slider
+	--------------------------------------*/
+	if (jQuery(".slider-active-wrap .swiper-container").length > 0) {
+		let sliderActive1 = '.slider-active-wrap';
+		let sliderInit1 = new Swiper(sliderActive1, {
+			// Optional parameters
+			slidesPerView: 1,
+			slidesPerColumn: 1,
+			paginationClickable: true,
+			loop: true,
+			effect: 'fade',
+
+			autoplay: {
+				delay: 5000,
+			},
+
+			// If we need pagination
+			pagination: {
+				el: '.swiper-pagination',
+				type: 'fraction',
+				// clickable: true,
+			},
+
+			// Navigation arrows
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev',
+			},
+
+			a11y: false
+		});
+
+		function animated_swiper(selector, init) {
+			let animated = function animated() {
+				$(selector + ' [data-animation]').each(function () {
+					let anim = $(this).data('animation');
+					let delay = $(this).data('delay');
+					let duration = $(this).data('duration');
+
+					$(this).removeClass('anim' + anim)
+						.addClass(anim + ' animated')
+						.css({
+							webkitAnimationDelay: delay,
+							animationDelay: delay,
+							webkitAnimationDuration: duration,
+							animationDuration: duration
+						})
+						.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
+							$(this).removeClass(anim + ' animated');
+						});
+				});
+			};
+			animated();
+			// Make animated when slide change
+			init.on('slideChange', function () {
+				$(sliderActive1 + ' [data-animation]').removeClass('animated');
+			});
+			init.on('slideChange', animated);
+		}
+
+		animated_swiper(sliderActive1, sliderInit1);
+	}
+
+	/*------------------------------------
+        slick-Slider
+	--------------------------------------*/
+
+	$('.center').slick({
+		centerMode: true,
+		centerPadding: '60px',
+		slidesToShow: 4,
+		responsive: [
+		  {
+			breakpoint: 768,
+			settings: {
+			  arrows: false,
+			  centerMode: true,
+			  centerPadding: '40px',
+			  slidesToShow: 4
+			}
+		  },
+		  {
+			breakpoint: 480,
+			settings: {
+			  arrows: false,
+			  centerMode: true,
+			  centerPadding: '40px',
+			  slidesToShow: 1
+			}
+		  }
+		]
+	  });
+
+
+	$('.study').slick({
+		centerMode: true,
+		centerPadding: '60px',
+		slidesToShow: 3,
+		responsive: [
+		  {
+			breakpoint: 768,
+			settings: {
+			  arrows: true,
+			  centerMode: true,
+			  centerPadding: '40px',
+			  slidesToShow: 3
+			}
+		  },
+		  {
+			breakpoint: 480,
+			settings: {
+			  arrows: true,
+			  centerMode: true,
+			  centerPadding: '40px',
+			  slidesToShow: 1
+			}
+		  }
+		]
+	  });
+
+
+
+})(jQuery);
